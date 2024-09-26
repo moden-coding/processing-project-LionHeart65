1fa715059aa14eb2aaaaa13a57acfedd31e9ac18
Ari Yellin-Levine <arilev65@gmail.com>
2024-09-26 14:32:12 -0400

    pretty funny misitake. (and setting up the starting char and movement)

--- src/App.java
+++ src/App.java
@@ -1,20 +1,33 @@
-import processing.core.*;
+import processing.core.PApplet;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    int charX = 0;
+    int charY = 0;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
+    public void setup() {
+        charX = width / 2;
+        charY = height / 2;
+        background(255);
+
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(750, 500);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        charAndWea();
+
+    }
+
+    public void charAndWea() {
+        rect(charX, charY, 20, 50);
+        ellipse(charX + 10, charY, 15, 15);
+        rect(charX + 5, charY + 25, -30, 5);
 
     }
 }
