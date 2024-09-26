0e211403fd15ebb96cd4053c1000258b6f1e5b4a
Ari Yellin-Levine <arilev65@gmail.com>
2024-09-26 16:05:06 -0400

    me no trust robery

--- src/App.java
+++ src/App.java
@@ -1,17 +1,22 @@
 import processing.core.PApplet;
 
 public class App extends PApplet {
+
     int charX = 0;
     int charY = 0;
-
-    public static void main(String[] args) {
-        PApplet.main("App");
-    }
+    int dir = 1;
+    float speed = 10;
+    color bg = color(255, 255, 255);
+    float shootAngle = 0;
+    int shootX = 0;
+    int shootY = 0;
+    float bulletSpeed = 100;
+    
 
     public void setup() {
         charX = width / 2;
         charY = height / 2;
-        background(255);
+        background(bg);
 
     }
 
@@ -20,14 +25,50 @@ public class App extends PApplet {
     }
 
     public void draw() {
+        background(bg);
         charAndWea();
 
     }
 
     public void charAndWea() {
+    
         rect(charX, charY, 20, 50);
         ellipse(charX + 10, charY, 15, 15);
-        rect(charX + 5, charY + 25, -30, 5);
+        // rect(charX + 5, charY + 20, 30 * dir, 5);
+        float angle = atan2(mouseY - (charX + 5), mouseX - (charY + 20));
+        translate((charX + 5), (charY + 20));
+        rotate(angle);
+        rect(0, 0, 30*dir, 5);
+        shootAngle = atan2(((0 - 30)/ 2), ((0 + 30)/ 2));
+        resetMatrix();
+        shootX = charX + 5;
+        shootY = charY + 20;
+        // TODO fix weird drift
+        
+    }
+    public void shoot() {
+        fill(0);
+        rect(shootX, shootY, 5, 5);
 
+        fill(255);
+        
+    }
+
+    public void keyPressed() {
+        if (key == 'w') {
+            charY -= speed;
+
+        } else if (key == 's') {
+            charY += speed;
+
+        } else if (key == 'a') {
+            charX -= speed;
+            dir = -1;
+
+        } else if (key == 'd') {
+            charX += speed;
+            dir = 1;
+
+        }
     }
 }
